659ee156166a5ac2725c13d2e6c440bb9a368b31
cclutta <52520502+cclutta@users.noreply.github.com>
2022-05-27 15:11:38 +0300

    Update main.c

--- main.c
+++ main.c
@@ -12,45 +12,44 @@ void exec_command(char *comm)
 	char *argv[3];
 	char exit_c[5] = "exit";
 
-	if(strcmp(exit_c, comm) == 0)
+	if (strcmp(exit_c, comm) == 0)
 	{
 	printf("Goodbye");
 	exit(1);
 	}
-	
+
 	strcat(path, comm);
-		
+	
 	argv[0] = path;
 	argv[1] = NULL;
-	
+
 	pid = fork();
-	
-	if(pid == -1)
+
+	if (pid == -1)
 	{
 		printf("\nfailed fork");
 		return;
-	} 
+	}
 	else if (pid == 0)
 	{
-        if (execve(argv[0], argv, NULL) < 0)
-	{
-            printf("\nCould not execute command\n");
-        }
-        exit(0);
+		if (execve(argv[0], argv, NULL) < 0)
+		{
+			printf("\nCould not execute command\n");
+		}
+		exit(0);
 	}
 	else 
 	{
-        wait(NULL); 
-        return;
-    }
-		
+		wait(NULL);
+		return;
+	}		
 }
 
 /**
 * get_null_bytes - get the null bytes
 * @str: string
 * @index: the index
-* 
+*
 * Return: void
 */
 void get_null_bytes(char *str, unsigned int index)
@@ -78,31 +77,25 @@ exit(1);
 }
 
 
-while (1){
-printf("$ ");
-chars = getline(&buffer, &bufsize, stdin);
-
-if(chars == -1)
+while (1)
 {
- if (feof(stdin))
- {
-      exit(EXIT_SUCCESS);
-    } 
-else  
-{
-      perror("readline");
-      exit(EXIT_FAILURE);
-    }
-  }
-
-
-get_null_bytes(buffer, chars - 1);
-
-exec_command(buffer);
-
+	printf("$ ");
+	chars = getline(&buffer, &bufsize, stdin);
+	if(chars == -1)
+	{
+		if (feof(stdin))
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("readline");
+			exit(EXIT_FAILURE);
+		}
+	}
+	get_null_bytes(buffer, chars - 1);
+	exec_command(buffer);
 }
-
-free(buffer);
-
-return (0);
+	free(buffer);
+	return (0);
 }
